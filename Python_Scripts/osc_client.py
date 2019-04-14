27b76b1092a09c019a78af24f66993374d353a69
sylatupa <sylatupa@asu.edu>
2019-04-13 17:59:34 -0700

    args

--- Python_Scripts/osc_client.py
+++ Python_Scripts/osc_client.py
@@ -1,13 +1,22 @@
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 import time
+import argparse
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
+
+print(args)
+
 ip = "192.168.1.115"
 port = 4000
 
 client = udp_client.SimpleUDPClient(ip, port)
 
 def knobs(message):
+    print(message)
     for i in range(0,1000):
         client.send_message("/knobs", [i,i,i,i,i,i])
         time.sleep(.07)
 knobs('here')
+
+
