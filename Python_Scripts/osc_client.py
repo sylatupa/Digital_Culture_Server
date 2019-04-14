da5353e6e4acbaba91f55b61eac475618590441e
sylatupa <sylatupa@asu.edu>
2019-04-13 19:13:58 -0700

    OSC helper script to run in node-red

--- Python_Scripts/osc_client.py
+++ Python_Scripts/osc_client.py
@@ -5,22 +5,69 @@ import sys
 import argparse
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--program", "-p", help="set output width")
+parser.add_argument("--next", "-n", help="set output width")
 parser.add_argument("--knob", "-k", help="set output width")
+parser.add_argument("--trigger", "-t", help="set output width")
+parser.add_argument("--osd", "-o", help="set output width")
+parser.add_argument("--screen", "-s", help="set output width")
+parser.add_argument("--autoclear", "-a", help="set output width")
 args = parser.parse_args()
 
-print(args.program)
+print(args.next)
+print(args.knob)
+print(args.trigger)
+print(args.osd)
+print(args.screen)
+print(args.autoclear)
 
 ip = "192.168.1.115"
 port = 4000
 
 client = udp_client.SimpleUDPClient(ip, port)
 
+def next(message):
+    #number must be in range of all available modes
+    client.send_message("/key", [2,int(message)])
+
 def knobs(message):
-    print(message)
-    for i in range(message,message+1):
-        client.send_message("/knobs", [i,i,i,i,i,i])
-        time.sleep(.07)
-knobs(int(args.knob))
+    #
+    i = int(message)
+    client.send_message("/knobs", [i,i,i,i,i,i])
+
+def osd(message):
+    # toggle on and off
+    i = int(message)
+    client.send_message("/key", [3,i])
+
+def trigger(i):
+    # positive turn on, negative turns off
+    client.send_message("/key", [9,i])
+ 
+def screen(i):
+    # positive turn on
+    client.send_message("/key", [7,i])
+  
+def autoclear(i):
+    # positive turn off negative on
+    client.send_message("/key", [8,i])
+    
+   
+   
+
+if args.knob:
+    knobs(int(args.knob))
+if args.osd:
+    osd(int(args.osd))
+if args.trigger:
+    trigger(int(args.trigger))
+if args.next:
+    next(int(args.next))
+if args.screen:
+    next(int(args.screen))
+if args.autoclear:
+    autoclear(int(args.autoclear))
+
+
+
 
 
